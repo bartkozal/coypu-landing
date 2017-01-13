bb74fa246f592453d4538fa4a87f2822b42c8159
Bartlomiej Kozal <bkzl@me.com>
2017-01-13 03:55:00 +0100

    Use plugin to load Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAnalytics, { loadScript } from 'vue-analytics'
 import App from './App'
+
+Vue.use(VueAnalytics)
+
+loadScript('UA-51355575-8')
 
 /* eslint-disable no-new */
 new Vue({
