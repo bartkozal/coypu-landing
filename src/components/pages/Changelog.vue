3723e1d1b01fadd099f1b6caa1717445ff11eb51
Bartlomiej Kozal <bkzl@me.com>
2017-01-15 20:02:14 +0100

    Release v1.0.1

--- src/components/pages/Changelog.vue
+++ src/components/pages/Changelog.vue
@@ -7,7 +7,7 @@
         <div class="grid-item 1/2 text-small text-muted">{{ release.date }}</div>
       </div>
       <hr class="margin-1/4-top margin-1/2-bottom">
-      <p>{{ release.body }}</p>
+      <p v-html="release.body"></p>
     </div>
 </template>
 
@@ -22,6 +22,19 @@ export default {
   data () {
     return {
       releases: [
+        {
+          version: 'v1.0.1',
+          date: 'Jan 15th, 2017',
+          body: `- Make <strong>Enter</strong> and <strong>Backspace</strong>]
+            keys depend on caret position (so, it's possible now to split, join
+            or insert new task above existing)<br>
+          - Allow to deselect tasks with <strong>Esc</strong><br>
+          - Remember application window position and size between runs<br>
+          - All labels (week number, week range and day) are now clickable<br>
+          - Highlight current day<br>
+          - Improve performance<br>
+          - Adjust the icon size to match macOS guidelines`
+        },
         {
           version: 'v1.0.0',
           date: 'Jan 10th, 2017',
