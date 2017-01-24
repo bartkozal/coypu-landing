35c0c916a5476036e29f03be4ee7680c4eabe6f0
Bartlomiej Kozal <bkzl@me.com>
2017-01-24 03:50:06 +0100

    Fix GA code

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import VueAnalytics, { loadScript } from 'vue-analytics'
 import App from './App'
 import router from './router'
 
 Vue.use(VueRouter)
-Vue.use(VueAnalytics)
-
-loadScript('UA-51355575-8')
 
 /* eslint-disable no-new */
 new Vue({
