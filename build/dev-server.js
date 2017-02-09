1a6f2d7d16251584f0554e35e7c681d0cd547d90
Bartlomiej Kozal <bkzl@me.com>
2017-02-09 13:06:03 +0100

    New demo

--- build/dev-server.js
+++ build/dev-server.js
@@ -53,8 +53,7 @@ app.use(devMiddleware)
 app.use(hotMiddleware)
 
 // serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+app.use(express.static('public'))
 
 var uri = 'http://localhost:' + port
 
