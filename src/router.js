4659c2eecde34bef2792c11d29c9324596723ca7
Bartlomiej Kozal <bkzl@me.com>
2017-02-14 17:43:48 +0100

    Use vue-ga

--- src/router.js
+++ src/router.js
@@ -2,11 +2,16 @@ import VueRouter from 'vue-router'
 import Home from 'components/pages/home'
 import FAQ from 'components/pages/faq'
 import Changelog from 'components/pages/changelog'
+import ga from 'vue-ga'
 
-export default new VueRouter({
+const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/changelog', component: Changelog },
     { path: '/faq', component: FAQ }
   ]
 })
+
+ga(router, 'UA-51355575-8')
+
+export default router
